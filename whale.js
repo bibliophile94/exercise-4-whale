e11e645e47020de87fcf0d59c86381ddebfd3aaf
bibliophile94 <kstolpe@mail.bradley.edu>
2015-10-18 23:59:51 -0500

    finished moby dick

--- whale.js
+++ whale.js
@@ -1,11 +1,17 @@
 jQuery(document).ready(function($) {
-var num = parseInt(location.hash.substr(1));
-console.log(num);
-if (isNaN(num)) {
-    num = 0;
-}
-$.get('/whale/txt/' + num + '.txt', function(txt)) {
-    $('a').html(txt);
-	$('a').attr('href', '#' + (num + 1));
-});
+    function update() {
+		jQuery(document).ready(function($) {
+		var num = parseInt(location.hash.substr(1));
+		console.log(num);
+		if (isNaN(num)) {
+			num = 0;
+		}
+		$.get('/whale/txt/' + num + '.txt', function(txt)) {
+			$('a').html(txt);
+			$('a').attr('href', '#' + (num + 1));
+		});
+		});
+	}
+	update();
+	window.addEventListener('hashchange', update, false);
 });
